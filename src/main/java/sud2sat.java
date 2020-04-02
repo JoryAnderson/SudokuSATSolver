4c17f811ebcebbb63dfe354a127e2639ca80e015
Jory Anderson <Jory2554@gmail.com>
2020-04-02 15:36:08 -0700

    Fixed clause comment Updated extended report

--- src/main/java/sud2sat.java
+++ src/main/java/sud2sat.java
@@ -32,7 +32,7 @@ public class sud2sat {
 
     }
 
-    outputList.add("\nc One number in each entry\n");
+    outputList.add("\nc There is at least one number in each entry\n");
     for(int x = 1; x < 10; x++) {
       for(int y = 1; y < 10; y++) {
         for(int z = 1; z < 10; z++) {
